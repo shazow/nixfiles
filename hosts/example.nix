9ee1585b980b37987c712fc92678cb98a8a8d31e
Andrey Petrov <shazow@gmail.com>
2018-11-18 12:49:41 -0500

    Create example.nix

---
+++ hosts/example.nix
@@ -0,0 +1,18 @@
+{ config, pkgs, ... }:
+{
+  boot.blacklistedKernelModules = [ "mei_me" ];
+  imports = [
+    ./common.nix
+    <nixos-hardware/lenovo/thinkpad/x1/6th-gen/QHD>
+  ];
+  networking.hostName = "examplehost";
+  
+  users.extraUsers.exampleuser = {
+    isNormalUser = true;
+    home = "/home/exampleuser";
+    description = "Example";
+    extraGroups = [ "wheel" "sudoers" "audio" "video" "disk" "networkmanager"];
+    uid = 1000;
+    hashedPassword = let hashedPassword = import ./.hashedPassword.nix; in hashedPassword; # Make with mkpasswd
+  };
+}
