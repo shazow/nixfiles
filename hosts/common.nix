9d1b56b1c898193721d850118a85c1d77b6b74d3
Andrey Petrov <shazow@gmail.com>
2018-11-18 12:36:28 -0500

    Update common.nix

--- hosts/common.nix
+++ hosts/common.nix
@@ -1,1 +1,2 @@
-# TODO: I want a setup where all my different machine-specific configs are in hosts/*.nix, and host.nix is a symlink or import of a specific host.
+# TODO: I want a setup where all my different machine-specific configs are in hosts/*.nix,
+# and host.nix is a symlink or import of a specific host. Hardware configs will live here.
