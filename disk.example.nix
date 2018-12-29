d0de6f1c68e07f19b4be7584295a16175a05b443
Andrey Petrov <andrey.petrov@shazow.net>
2018-12-29 14:09:07 -0500

    Update disk.example.nix

--- disk.example.nix
+++ disk.example.nix
@@ -2,7 +2,7 @@
 {
   extraInitrd = ./initrd.keys.gz;
   keyFile = "cryptroot.key";
-  efi = /dev/nvme0n1p1;
-  cryptswap = /dev/nvme0n1p2;
-  cryptroot = /dev/nvme0n1p3;
+  efi = "/dev/nvme0n1p1";
+  cryptswap = "/dev/nvme0n1p2";
+  cryptroot = "/dev/nvme0n1p3";
 }
