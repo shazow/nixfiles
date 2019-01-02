55ae8f2143accd48d66bb259fd004c027d6095c5
Andrey Petrov <andrey.petrov@shazow.net>
2019-01-02 10:33:33 -0500

    Update shazowic-ghost.nix

--- hosts/shazowic-ghost.nix
+++ hosts/shazowic-ghost.nix
@@ -1,24 +1,87 @@
-{ config, lib, ... }:
+let hashedPassword = import ./.hashedPassword.nix; in  # Make with mkpasswd
+
+{ config, pkgs, lib, ... }:
+
 {
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" ];
   boot.blacklistedKernelModules = [ "mei_me" ];
-  boot.initrd.availableKernelModules = [ "ata_piix" "ohci_pci" "ahci" "sd_mod" "sr_mod" ];
-  nix.maxJobs = lib.mkDefault 2;
-  virtualisation.virtualbox.guest.enable = true;
+  hardware.enableAllFirmware = true;
+
+  nix.maxJobs = lib.mkDefault 8;
+  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
 
   imports = [
+    ./common/hardware-thinkpad-x1c.nix
     ./common/boot.nix
-    ./common/server.nix
+    ./common/desktop.nix
   ];
-  networking.hostName = "shazowic-ghost";
 
-  users.extraUsers.shazow = {
+  environment.systemPackages = with pkgs; [
+    # System
+    acpi
+    binutils-unwrapped
+    fd
+    fwupd
+    htop
+    lm_sensors
+    p7zip
+    pciutils
+    powertop
+    psmisc
+    ripgrep
+    sysstat
+    tree
+    unzip
+
+    # Desktop
+    alsaTools
+    blueman
+    networkmanagerapplet
+    clipnotify
+    dunst
+    feh
+    i3lock
+    i3status-rust
+    libnotify
+    maim
+    pavucontrol
+    pcmanfm
+    rofi
+    xss-lock
+    xsel
+
+    # Apps
+    alacritty
+    cargo
+    discord
+    gnupg
+    go
+    google-chrome-beta
+    python3
+    signal-desktop
+    vlc
+
+    # Fonts
+    dejavu_fonts
+    font-awesome-ttf
+  ];
+  services.clipmenu.enable = true;
+  services.dnsmasq.enable = true;
+  services.dnsmasq.servers = [ "1.1.1.1" "8.8.8.8" "8.8.4.4" ];
+
+  networking.hostName = "shazowic-corvus";
+
+  users.users.shazow = {
     isNormalUser = true;
     home = "/home/shazow";
     description = "shazow";
-    extraGroups = [ "wheel" "sudoers" "audio" "video" "disk" "networkmanager"];
+    extraGroups = [ "wheel" "sudoers" "audio" "video" "disk" "networkmanager" "plugdev"];
     uid = 1000;
-    hashedPassword = let hashedPassword = import ./.hashedPassword.nix; in hashedPassword; # Make with mkpasswd
+    hashedPassword = hashedPassword;
   };
+
+  boot.loader.grub.extraEntries = import ./archboot.nix;
 
   # This value determines the NixOS release with which your system is to be
   # compatible, in order to avoid breaking some software such as database
