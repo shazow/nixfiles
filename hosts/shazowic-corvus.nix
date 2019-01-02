0c269ae5d4e685a0a0af4185ca75db7e4c6960eb
Andrey Petrov <andrey.petrov@shazow.net>
2019-01-02 10:36:06 -0500

    Update shazowic-corvus.nix

--- hosts/shazowic-corvus.nix
+++ hosts/shazowic-corvus.nix
@@ -1,17 +1,74 @@
-{ config, lib, ... }:
+let hashedPassword = import ./.hashedPassword.nix; in  # Make with mkpasswd (see Makefile)
+
+{ config, pkgs, lib, ... }:
 
 {
+  boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" ];
   boot.blacklistedKernelModules = [ "mei_me" ];
+  hardware.enableAllFirmware = true;
 
   nix.maxJobs = lib.mkDefault 8;
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
 
   imports = [
-    <nixos-hardware/lenovo/thinkpad/x1/6th-gen/QHD>
+    ./common/hardware-thinkpad-x1c.nix
     ./common/boot.nix
     ./common/desktop.nix
   ];
+
+  environment.systemPackages = with pkgs; [
+    # System
+    acpi
+    binutils-unwrapped
+    fd
+    fwupd
+    htop
+    lm_sensors
+    p7zip
+    pciutils
+    powertop
+    psmisc
+    ripgrep
+    sysstat
+    tree
+    unzip
+
+    # Desktop
+    alsaTools
+    blueman
+    networkmanagerapplet
+    clipnotify
+    dunst
+    feh
+    i3lock
+    i3status-rust
+    libnotify
+    maim
+    pavucontrol
+    pcmanfm
+    rofi
+    xss-lock
+    xsel
+
+    # Apps
+    alacritty
+    cargo
+    discord
+    gnupg
+    go
+    google-chrome-beta
+    python3
+    signal-desktop
+    vlc
+
+    # Fonts
+    dejavu_fonts
+    font-awesome-ttf
+  ];
+  services.clipmenu.enable = true;
+  services.dnsmasq.enable = true;
+  services.dnsmasq.servers = [ "1.1.1.1" "8.8.8.8" "8.8.4.4" ];
 
   networking.hostName = "shazowic-corvus";
 
@@ -21,28 +78,10 @@
     description = "shazow";
     extraGroups = [ "wheel" "sudoers" "audio" "video" "disk" "networkmanager" "plugdev"];
     uid = 1000;
-    hashedPassword = let hashedPassword = import ./.hashedPassword.nix; in hashedPassword; # Make with mkpasswd
+    hashedPassword = hashedPassword;
   };
 
-  services.tlp.extraConfig = ''
-    START_CHARGE_THRESH_BAT0=75
-    STOP_CHARGE_THRESH_BAT0=90
-    CPU_SCALING_GOVERNOR_ON_BAT=powersave
-    ENERGY_PERF_POLICY_ON_BAT=powersave
-  '';
-
-  # TODO: Figure out how to disable these...
-  # Disable the "throttling bug fix" -_- https://github.com/NixOS/nixos-hardware/blob/master/common/pc/laptop/cpu-throttling-bug.nix
-  #systemd.timers.cpu-throttling.enable = false;
-  #systemd.services.cpu-throttling.enable = false;
-
-  i18n = {
-    consoleFont = "Lat2-Terminus16";
-    consoleKeyMap = "us";
-    defaultLocale = "en_US.UTF-8";
-  };
-
-  time.timeZone = "America/Toronto";
+  boot.loader.grub.extraEntries = import ./archboot.nix;
 
   # This value determines the NixOS release with which your system is to be
   # compatible, in order to avoid breaking some software such as database
