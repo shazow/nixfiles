7359efad2645cce7ffe5c015d1220865f07da5f5
Andrey Petrov <andrey.petrov@shazow.net>
2020-02-02 12:37:51 -0500

    Add desktop

---
+++ shazowic-beast.nix
@@ -0,0 +1,109 @@
+let hashedPassword = import ./.hashedPassword.nix; in  # Make with mkpasswd (see Makefile)
+
+{ config, pkgs, lib, ... }:
+
+{
+  services.localtime.enable = true;
+
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "sd_mod" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-intel" ];
+  boot.blacklistedKernelModules = [ "mei_me" ];
+  hardware.opengl.extraPackages = with pkgs; [ vaapiIntel libvdpau-va-gl vaapiVdpau ];
+
+
+  # Bluetooth
+  services.blueman.enable = true;
+  hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = false;
+
+  nix.maxJobs = lib.mkDefault 4;
+  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
+
+  imports = [
+    ./common/boot.nix
+    ./common/desktop-i3.nix
+  ];
+
+  # Hardware specific
+  hardware.cpu.intel.updateMicrocode = true;
+  hardware.enableRedistributableFirmware = true;
+  services.xserver.videoDrivers = [ "nvidia" ];
+  services.fwupd.enable = true;
+  networking.interfaces.enp0s31f6.useDHCP = true;
+  networking.interfaces.wlp0s20f0u12.useDHCP = true;
+
+
+  networking.firewall.allowedTCPPorts = [
+    8010  # VLC Chromecast
+  ];
+
+  environment.systemPackages = with pkgs; [
+    home-manager
+
+    # Desktop
+    alsaTools
+    arandr
+    colord
+    dunst
+    feh
+    libnotify
+    maim
+    openvpn
+    pavucontrol
+    xclip
+    xdotool
+    xsel
+
+    # Apps
+    gnupg
+
+    # Other
+    android-udev-rules
+    alsa-firmware
+  ];
+
+  services.dnsmasq.enable = true;
+  services.dnsmasq.servers = [ "1.1.1.1" "8.8.8.8" "2001:4860:4860::8844" ];
+  #networking.networkmanager.appendNameservers = [ "1.1.1.1" "8.8.8.8" "2001:4860:4860::8844" ];
+  networking.hostName = "shazowic-beast";
+  networking.networkmanager.wifi.backend = "iwd";
+  networking.networkmanager.wifi.macAddress = "permanent";  # One of "preserve", "random", "stable", "permanent", "00:11:22:33:44:55"
+
+  virtualisation.docker = {
+    enable = true;
+    enableOnBoot = false; # Started on-demand by docker.socket
+  };
+
+  users.users.shazow = {
+    isNormalUser = true;
+    home = "/home/shazow";
+    description = "shazow";
+    extraGroups = [ "wheel" "sudoers" "audio" "video" "disk" "networkmanager" "plugdev" "adbusers" "docker"];
+    uid = 1000;
+    hashedPassword = hashedPassword;
+  };
+
+  users.users.andrey = {
+    isNormalUser = true;
+    home = "/home/andrey";
+    description = "andrey";
+    uid = 1100;
+    hashedPassword = hashedPassword;
+  };
+
+  # Agent daemon required for pinentry
+  programs.gnupg.agent.enable = true;
+
+  # Android dev
+  programs.adb.enable = true;
+
+  boot.loader.grub.extraEntries = import ./.extraboot.nix;
+
+  # This value determines the NixOS release with which your system is to be
+  # compatible, in order to avoid breaking some software such as database
+  # servers. You should change this only after NixOS release notes say you
+  # should.
+  system.stateVersion = "20.03";
+}
