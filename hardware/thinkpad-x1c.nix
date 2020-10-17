5947af9f8b665407f8ff0844541d61dfb2180eda
Andrey Petrov <andrey.petrov@shazow.net>
2020-10-17 19:54:43 -0400

    hardware/thinkpad-x1c.nix: Switch back to nixos-hardware

--- hardware/thinkpad-x1c.nix
+++ hardware/thinkpad-x1c.nix
@@ -1,18 +1,12 @@
-{ config, pkgs, lib, ... }:
+{ pkgs, lib, ... }:
 
 {
+  imports = [
+    <nixos-hardware/lenovo/thinkpad/x1/6th-gen>
+  ];
+
   hardware.cpu.intel.updateMicrocode = true;
   hardware.enableRedistributableFirmware = true; # Might help wifi?
-
-  # Backport from <nixos-hardware/lenovo/thinkpad/x1/6th-gen/QHD>
-  boot.kernelModules = [ "acpi_call" ];
-  boot.extraModulePackages = with config.boot.kernelPackages; [ acpi_call ];
-
-  # Fix sizes of GTK/GNOME ui elements
-  #environment.variables = {
-  #  GDK_SCALE = lib.mkDefault "2";
-  #  GDK_DPI_SCALE= lib.mkDefault "0.5";
-  #};
 
   services.xserver.dpi = 210; # 210 is the native DPI of the HDR screen
   fonts.fontconfig.dpi = 140; # This is Xft.dpi in .Xresources, 140 = 210 / 1.5
@@ -52,12 +46,13 @@
     DEVICES_TO_DISABLE_ON_STARTUP="bluetooth"
     DEVICES_TO_DISABLE_ON_BAT_NOT_IN_USE="bluetooth"
   '';
-  config.services.tlp.settings.cpuFreqGovernor = "powersave";
+  services.tlp.settings.cpuFreqGovernor = "powersave";
 
   # Firmware updating
   services.fwupd.enable = true;
 
   # Disable the "throttling bug fix" -_- https://github.com/NixOS/nixos-hardware/blob/master/common/pc/laptop/cpu-throttling-bug.nix
+  services.throttled.enable = false;
   systemd.timers.cpu-throttling.enable = lib.mkForce false;
   systemd.services.cpu-throttling.enable = lib.mkForce false;
 }
