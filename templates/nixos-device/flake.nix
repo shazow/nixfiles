fb721d1583b43ce9cb20e80cd35e8b33a563e593
Andrey Petrov <andrey.petrov@shazow.net>
2023-06-26 14:20:35 -0400

    templates/nixos-device: Add disk layout override

--- templates/nixos-device/flake.nix
+++ templates/nixos-device/flake.nix
@@ -1,5 +1,6 @@
 {
   inputs = {
+    # FIXME: Replace this with an absolute path
     nixfiles.url = "../../";
   };
 
@@ -7,10 +8,22 @@
     # FIXME: Replace this
     # $ mkpasswd hunter2
     initialHashedPassword = "$y$j9T$cKCdiliVyUYFTz6b6YC2K.$kLjtBxrCuzuxS//eMSLsHtXCkgkWimKq00cRdLzNBBB";
+
+    # Example of additional disk layout configs can be overridden below:
+    # disk = {
+    #   efi = { device = "/dev/disk/by-id/..."; };
+    #   luksDevices = {
+    #     cryptmedia = { device = "/dev/disk/by-id/..."; };
+    #   };
+    #   extraFileSystems = {
+    #     "/mnt/media" = { device = "/dev/mapper/cryptmedia"; fsType = "btrfs"; };
+    #   };
+    # };
   in {
     nixosConfigurations = nixfiles.mkSystemConfigurations {
       inherit (nixfiles) devices;
       inherit initialHashedPassword;
+      # inherit disk;
     };
   };
 }
