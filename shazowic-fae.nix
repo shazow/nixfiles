ea758f4d9d687234e807cecc47dbe630335880fd
Andrey Petrov <andrey.petrov@shazow.net>
2023-11-21 19:26:07 -0500

    hardware: Move framework-specific stuff to its own file

--- shazowic-fae.nix
+++ shazowic-fae.nix
@@ -1,21 +1,13 @@
 {
-  pkgs, lib,
+  pkgs,
   initialHashedPassword,
   disk,
   ...
 }:
 {
-  boot.kernelPackages = pkgs.linuxPackages_latest;
-
-  # $ nixos-generate-config --show-hardware-config
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "thunderbolt" "usb_storage" "sd_mod" ];
-  boot.kernelModules = [ "kvm-amd" ];
-
-  # TODO: Investigate: services.auto-cpufreq.enable = true;
-  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
-  powerManagement.powertop.enable = true; # Run powertop on boot
 
   imports = [
+    ./hardware/framework-13-amd.nix
     (import ./common/boot.nix {
       inherit disk;
     })
@@ -23,25 +15,6 @@
     ./common/crypto.nix
     ./common/guest.nix
   ];
-
-  # Hardware specific
-  hardware.cpu.amd.updateMicrocode = true;
-  hardware.enableRedistributableFirmware = true;
-  hardware.opengl.extraPackages = [
-    pkgs.rocmPackages.clr.icd
-    pkgs.amdvlk
-    # Encoding/decoding acceleration
-    pkgs.libvdpau-va-gl pkgs.vaapiVdpau
-  ];
-  hardware.opengl.extraPackages32 = [ ];
-  hardware.bluetooth.enable = true;
-  hardware.bluetooth.powerOnBoot = false;
-  services.xserver.videoDrivers = [ "amdgpu" ];
-  services.fwupd.enable = true;
-  services.fwupd.extraRemotes = [ "lvfs-testing" ]; # Some framework firmware is still in testing
-  services.blueman.enable = true;
-  services.fprintd.enable = true;
-  services.power-profiles-daemon.enable = true;
 
   environment.systemPackages = with pkgs; [
     home-manager
