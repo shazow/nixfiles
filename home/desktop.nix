3483c956ae0d1d8faab20a86e93460e387dc74c8
Andrey Petrov <andrey.petrov@shazow.net>
2024-01-05 14:43:11 -0500

    home/desktop.nix: New monitor

--- home/desktop.nix
+++ home/desktop.nix
@@ -15,7 +15,8 @@
 
   xresources.properties = {
     # Doesn't seem like most things need this, but flatpak electron apps do.
-    "Xft.dpi" = 163; # 3840x2160 over 27"
+    # "Xft.dpi" = 163; # 3840x2160 over 27"
+    "Xft.dpi" = 110; # 3440x1440 over 34"
   };
 
   home.file.".config/i3/config".source = ./config/i3/config;
