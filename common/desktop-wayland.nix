b17db9aa19b875045383af6486a7fd460ee9cf12
Andrey Petrov <andrey.petrov@shazow.net>
2024-01-06 16:03:51 -0500

    common/desktop-wayland.nix: Run sway in a dbus session

--- common/desktop-wayland.nix
+++ common/desktop-wayland.nix
@@ -22,7 +22,7 @@
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd ${pkgs.sway}/bin/sway";
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd \"dbus-run-session sway\"";
         user = "greeter";
       };
     };
