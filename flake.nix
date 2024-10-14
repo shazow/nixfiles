1b6af1f91815de752d3d7108a8306507a80605a9
Andrey Petrov <andrey.petrov@shazow.net>
2024-10-14 19:55:23 -0400

    flake.nix: Pin home-manager to stable release

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
-    home-manager.url = "github:nix-community/home-manager";
+    home-manager.url = "github:nix-community/home-manager/release-24.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nixos-hardware.url = "github:nixos/nixos-hardware";
 
