f7609f97fb66246c8e479551addde616666b88f4
Andrey Petrov <andrey.petrov@shazow.net>
2024-10-15 20:29:01 -0400

    common/desktop-wayland.nix: Remove wayland overlay

--- common/desktop-wayland.nix
+++ common/desktop-wayland.nix
@@ -1,19 +1,5 @@
 { pkgs, inputs, ... }:
 {
-  nix.settings = {
-    # Wayland binary caches
-    trusted-public-keys = [
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
-    ];
-    substituters = [
-      "https://cache.nixos.org"
-      "https://nixpkgs-wayland.cachix.org"
-    ];
-  };
-
-  nixpkgs.overlays = [ inputs.nixpkgs-wayland.overlay ];
-
   imports = [
     ./desktop.nix
   ];
