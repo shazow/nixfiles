3a1be4d4f31a496d94e8c2914f61b0a51497cc08
Andrey Petrov <andrey.petrov@shazow.net>
2024-11-01 11:53:33 -0400

    config/overlays.nix: Remove nvim-lspconfig override

--- pkgs/nvim/config/overlays.nix
+++ pkgs/nvim/config/overlays.nix
@@ -1,21 +1,22 @@
 _:
 {
+  # Handy command to quickly check available version: 
+  # curl --silent "https://raw.githubusercontent.com/NixOS/nixpkgs/refs/heads/nixos-unstable/pkgs/applications/editors/vim/plugins/generated.nix" | grep -A6 "nvim-lspconfig" | grep -m1 version
   nixpkgs.overlays = [
-    # Fixes lspconfig bug
-    # TODO: Remove when https://github.com/neovim/nvim-lspconfig/pull/3390 is in unstable
-    (final: prev: {
-      vimPlugins = prev.vimPlugins.extend (final': prev': {
-        nvim-lspconfig = final.vimUtils.buildVimPlugin {
-          name = "nvim-lspconfig";
-          version = "2024-10-22";
-          src = final.fetchFromGitHub {
-            owner = "neovim";
-            repo = "nvim-lspconfig";
-            rev = "0d62a16429dba5fded93a076237079b81527e8f3";
-            sha256 = "sha256-/5gFgpWNik17gdi6cLcm/CTGiWQqfZJkZ7G/lZ3hpFA=";
-          };
-        };
-      });
-    })
+    # Override example:
+    #(final: prev: {
+    #  vimPlugins = prev.vimPlugins.extend (final': prev': {
+    #    nvim-lspconfig = final.vimUtils.buildVimPlugin {
+    #      name = "nvim-lspconfig";
+    #      version = "2024-10-22";
+    #      src = final.fetchFromGitHub {
+    #        owner = "neovim";
+    #        repo = "nvim-lspconfig";
+    #        rev = "0d62a16429dba5fded93a076237079b81527e8f3";
+    #        sha256 = "sha256-/5gFgpWNik17gdi6cLcm/CTGiWQqfZJkZ7G/lZ3hpFA=";
+    #      };
+    #    };
+    #  });
+    #})
   ];
 }
