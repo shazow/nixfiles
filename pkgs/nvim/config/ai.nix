ead22c68b375fbcd9b3a7e06c2fa6925ac47e9e8
Andrey Petrov <andrey.petrov@shazow.net>
2024-11-20 12:22:37 -0500

    config/ai.nix: wip adding codecompanion.nvim

--- pkgs/nvim/config/ai.nix
+++ pkgs/nvim/config/ai.nix
@@ -16,4 +16,22 @@
       provider = "copilot";
     };
   };
+
+  extraPlugins = with pkgs; [
+  # {
+  #   plugin = vimUtils.buildVimPlugin {
+  #     name = "olimorris/codecompanion.nvim";
+  #     src = fetchFromGitHub {
+  #       owner = "olimorris";
+  #       repo = "codecompanion.nvim";
+  #       rev = "873c4eb7e1d8002ac1248ce1ada0761be8185d07"; # 8.4.1
+  #       hash = "sha256-HGtaKsgM9+cUinDYWKthk9tTMbSIBsK/T/B9krW+drg=";
+  #     };
+  #   };
+  # }
+  ]);
+
+  extraConfigLua = ''
+    --require("codecompanion").setup()
+  '';
 }
