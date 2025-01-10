ac5dbd2a08ae0e86f76f6a735e0246a280c35456
Andrey Petrov <andrey.petrov@shazow.net>
2025-01-10 16:24:02 -0500

    nix flake update; migrate nvim changes

--- pkgs/nvim/config/ai.nix
+++ pkgs/nvim/config/ai.nix
@@ -7,13 +7,13 @@
   plugins = {
     # Copilot
     copilot-lua.enable = true;
-    copilot-lua.suggestion.enabled = false; # Required for copilot-cmp
-    copilot-lua.panel.enabled = false; # Required for copilot-cmp
+    copilot-lua.settings.suggestion.enabled = false; # Required for copilot-cmp
+    copilot-lua.settings.panel.enabled = false; # Required for copilot-cmp
     copilot-cmp.enable = true;
     copilot-chat.enable = true;
     avante.enable = true;
     avante.settings = {
-      provider = "copilot";
+      provider = "claude";
     };
   };
 
