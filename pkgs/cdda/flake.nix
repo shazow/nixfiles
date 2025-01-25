fc7c5316ea937d7c5b06a12029fc963a6e39ff0f
Andrey Petrov <andrey.petrov@shazow.net>
2025-01-25 12:19:54 -0500

    pkgs/cdda

--- pkgs/cdda/flake.nix
+++ pkgs/cdda/flake.nix
@@ -1,24 +1,39 @@
 {
   inputs = {
-    cdda-src = {
-      # Update with:
-      # $ nix flake update --override-input cdda-src github:CleverRaven/Cataclysm-DDA/0.H
-      # or
-      # $ nix flake update --override-input cdda-src github:CleverRaven/Cataclysm-DDA/$(curl https://api.github.com/repos/CleverRaven/Cataclysm-DDA/releases | jq -r .[0].tag_name)
-      url = "github:CleverRaven/Cataclysm-DDA";
-      flake = false;
-    };
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
-  outputs = { nixpkgs, flake-utils, cdda-src, ... }:
+
+  outputs = { nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = import nixpkgs { inherit system; };
     in {
-      packages.default = pkgs.cataclysm-dda-git.overrideAttrs (oldAttrs: {
-        version = cdda-src.lastModifiedDate;
-        src = cdda-src;
-        patches = [
-          ./locale-path-git.patch
-        ];
-      });
-    });
+      packages.default = pkgs.cataclysm-dda-git;
+    }
+  );
 }
+
+#{
+#  inputs = {
+#    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+#    cdda-src = {
+#      # Update with:
+#      # $ nix flake update --override-input cdda-src github:CleverRaven/Cataclysm-DDA/0.H-RELEASE
+#      # or
+#      # $ nix flake update --override-input cdda-src github:CleverRaven/Cataclysm-DDA/$(curl https://api.github.com/repos/CleverRaven/Cataclysm-DDA/releases | jq -r .[0].tag_name)
+#      url = "github:CleverRaven/Cataclysm-DDA";
+#      flake = false;
+#    };
+#  };
+#  outputs = { nixpkgs, flake-utils, cdda-src, ... }:
+#    flake-utils.lib.eachDefaultSystem (system: let
+#      pkgs = import nixpkgs { inherit system; };
+#    in {
+#      packages.default = pkgs.cataclysm-dda-git.overrideAttrs (oldAttrs: {
+#        version = cdda-src.lastModifiedDate;
+#        src = cdda-src;
+#        #patches = [
+#        #  ./locale-path-git.patch
+#        #];
+#      });
+#    });
+#}
