64abeffabeb31ab558b2922448a96073e18b29c5
Andrey Petrov <andrey.petrov@shazow.net>
2025-06-21 15:19:29 -0400

    vms: WIP

--- vms/shell-example/flake.nix
+++ vms/shell-example/flake.nix
@@ -21,9 +21,28 @@
           {
             microvm = {
               mem = 1024;
+              hotplugMem = 2048; # Extra memory that can be added if there's memory pressure
               storeOnDisk = true;
               writableStoreOverlay = "/nix/.rw-store";
               hypervisor = "cloud-hypervisor";
+              interfaces = [
+                {
+                  # macvtap is more efficient than a plain tap bridge (which inspects every packet at the kernel level)
+                  type = "macvtap";
+                  macvtap = {
+                    mode = "bridge"; # or "private" if we don't want VMs to talk to each other
+                    link = "eno1";
+                    ## Need to make a user-owned macvtap device first
+                    ## https://astro.github.io/microvm.nix/interfaces.html?highlight=macvtap#type--macvtap
+                    ## TODO: Make this declarative somehow?
+                    # sudo ip l add link $LINK name $ID type macvtap mode bridge
+                    # IFINDEX=$(cat /sys/class/net/$ID/ifindex)
+                    # sudo chown $USER /dev/tap$IFINDEX
+                  };
+                  id = "microvm1";
+                  mac = "02:00:00:00:00:01";
+                }
+              ];
             };
           }
           (
@@ -38,6 +57,16 @@
                   experimental-features = nix-command flakes
                 '';
               };
+
+              networking.useNetworkd = true;
+              systemd.network.enable = true;
+              systemd.network.networks."20-lan" = {
+                matchConfig.Type = "ether";
+                networkConfig = {
+                  DHCP = "yes";
+                };
+              };
+
             }
           )
         ];
