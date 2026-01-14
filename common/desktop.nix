99c61fc96bd7a9b88476c7faa62cd84b722b21c2
Andrey Petrov <andrey.petrov@shazow.net>
2026-01-14 15:10:38 -0500

    common/desktop.nix: Add libvirt to groups

--- common/desktop.nix
+++ common/desktop.nix
@@ -101,7 +101,7 @@
   users.users.${primaryUsername} = {
     isNormalUser = true;
     home = "/home/${primaryUsername}";
-    extraGroups = [ "wheel" "sudoers" "audio" "video" "disk" "networkmanager" "plugdev" "dialout" "adbusers" "docker" "i2c" "kvm" ];
+    extraGroups = [ "wheel" "sudoers" "audio" "video" "disk" "networkmanager" "plugdev" "dialout" "adbusers" "docker" "i2c" "kvm" "libvirt" ];
     uid = 1000;
     initialHashedPassword = initialHashedPassword;
   };
