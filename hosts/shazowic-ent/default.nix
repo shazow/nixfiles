34593b960df159af09ffcf09fba0eb9db610f634
Andrey Petrov <andrey.petrov@shazow.net>
2026-02-03 13:53:10 -0500

    hosts/shazowic-ent/default.nix: Import HD6XX EQ

--- hosts/shazowic-ent/default.nix
+++ hosts/shazowic-ent/default.nix
@@ -14,6 +14,14 @@
 
   home = [
     ../../home/desktop.nix
+
+    {
+      services.easyeffects.enable = true;
+
+      # Not sure if I like this EQ, need to experiment more
+      #services.easyeffects.preset = "Sennheiser-HD6XX-easyeffects"; # keys from easyeffects/output/*.json
+      xdg.configFile."easyeffects/output/HD6XX.json".source = ../../hardware/Sennheiser-HD6XX-easyeffects.json;
+    }
   ];
 
   root = import ./configuration.nix;
