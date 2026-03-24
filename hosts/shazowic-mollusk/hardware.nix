341205d0066958e7d6240813b0025a8c002c1e1d
Andrey Petrov <andrey.petrov@shazow.net>
2026-03-24 17:45:44 -0400

    hosts/shazowic-mollusk: Add hardware stubs

--- hosts/shazowic-mollusk/hardware.nix
+++ hosts/shazowic-mollusk/hardware.nix
@@ -1,9 +1,25 @@
-{ config, lib, pkgs, modulesPath, ... }:
+# This gets replaced on the fly by nixos-anywhere with --generate-hardware-config
+# but this stub should work too.
+{
+  config,
+  lib,
+  pkgs,
+  modulesPath,
+  ...
+}:
 
 {
-  imports =
-  [ (modulesPath + "/profiles/qemu-guest.nix")
+  imports = [
+    (modulesPath + "/profiles/qemu-guest.nix")
   ];
 
-  boot.initrd.availableKernelModules = [ "ahci" "uhci_hcd" "virtio_pci" "virtio_scsi" "sd_mod" "sr_mod" ];
+  boot.initrd.availableKernelModules = [
+    "ahci"
+    "uhci_hcd"
+    "virtio_pci"
+    "virtio_scsi"
+    "sd_mod"
+    "sr_mod"
+  ];
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 }
