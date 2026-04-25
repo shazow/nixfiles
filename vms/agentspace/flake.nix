a9af68c1a239ffdddaac9577d225acf3572ec709
Andrey Petrov <andrey.petrov@shazow.net>
2026-04-25 18:06:36 -0400

    vms/agentspace: Update to virtie flake

--- vms/agentspace/flake.nix
+++ vms/agentspace/flake.nix
@@ -12,8 +12,6 @@
       system = "x86_64-linux";
       statedir = "/home/shazow/vms/agentspace";
       sandbox = agentspace.lib.mkSandbox {
-        connectWith = "ssh";
-        protocol = "virtiofs";
         sshAuthorizedKeys = import ./authorizedKeys.nix;
         persistence.homeImage = "${statedir}/home.img";
         persistence.storeOverlay = "${statedir}/nix-store-overlay.img";
